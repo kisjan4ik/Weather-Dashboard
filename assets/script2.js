47a5efa0f6f93e1b0f88b5f01e6420fa47fffa40
Turetska <oturetska@yahoo.com>
2020-06-23 17:05:11 -0400

    organized js

--- assets/script2.js
+++ assets/script2.js
@@ -1,50 +1,62 @@
-function weather(searchCity) {
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" +
-        searchCity + "&appid=61a632379c3e9a3d1eecd3b47dea0b6b";
-
-    console.log(queryURL);
-
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    })
-
-        .then(function (response) {
-
-            // Log the queryURL
-            console.log(response);
-            UVindex(lat, lon);
-        })
-}
-
-function UVindex(lat, lon) {
-    var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=61a632379c3e9a3d1eecd3b47dea0b6b&lat=" + lat + "&lon=" + lon;
-
-    console.log(queryURL);
-
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-    })
-
-        .then(function (response) {
-
-            // Log the queryURL
-            console.log(response);
-
-        })
-}
-
-
-function iconWeather() {
-
-}
+var searchCity = $("#city").val();
+// var searchCity = $("#city").textContent;?????
+console.log(searchCity);
 
 $("button").on("click", function (event) {
     event.preventDefault();
 
-    var searchCity = $("form-control").val();
-    console.log(searchCity);
+
+    function weather(searchCity) {
+
+
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+            searchCity + "&appid=61a632379c3e9a3d1eecd3b47dea0b6b";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        })
+
+            .then(function (response) {
+
+                console.log(response);
+                UVindex(lat, lon);
+            })
+    }
+
+    function UVindex(lat, lon) {
+        var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=61a632379c3e9a3d1eecd3b47dea0b6b&lat=" + lat + "&lon=" + lon;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        })
+
+            .then(function (response) {
+
+                // Log the queryURL
+                console.log(response);
+
+            })
+    }
+
+
+    function iconWeather() {
+        var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" +
+            searchCity + "&appid=61a632379c3e9a3d1eecd3b47dea0b6b";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        })
+
+            .then(function (response) {
+
+                // Log the queryURL
+                console.log(response);
+
+            })
+    }
 
     weather(searchCity)
 
